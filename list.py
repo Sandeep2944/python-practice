17401438511d5bb5a715ff5b98c3118e41cf23d0
Sandeep M <sandeepm1817@gmail.com>
2025-12-10 16:57:18 +0530

    my list concept commit

---
+++ list.py
@@ -0,0 +1,32 @@
+a = []
+for i in dir(a):
+    if i[0] != "_":
+        print(i)
+
+
+
+########append
+a = [1,2]
+a.append(3)
+print(a)
+
+#####
+a = [1,2]
+a.append(9)
+print(a)
+
+#
+a = ["apple","banana","mango"]
+a.append("orange")
+print(a)
+
+
+# Start with an empty list
+numbers = []
+numbers.append(10)
+numbers.append(20)
+numbers.append(30)
+numbers.append(40)
+numbers.append(50)
+print(numbers)
+
